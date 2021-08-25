307c3d5bb4995ac4133ed425ed0135e19581d6f0
Shubh-RAZ <66317664+Shubh-RAZ@users.noreply.github.com>
2021-08-25 08:20:15 +0530

    directors note

---
+++ src/Components/DirectorsNote/DirectorsNote.js
@@ -0,0 +1,44 @@
+import './DirectorsNote.css'
+const DirectorsNote = () => {
+    return ( 
+        <div className="directorsnote">
+             <div className="note-heading">Directors Note</div>
+            <div className="inside-directors-note">
+               
+                <div className="directors-img">
+                    <img src="./director.jpg"></img>
+                </div>
+                <div className="director-note">
+                    <div className="directors-message">
+                        We give priority to the great ideas coming from all sides, I am with 'Awadh Vihar'.
+                        I welcome you to the College of Karmanparu, Ballia. It is an organization that deals with Ganuv×Tapanu[]
+                        high standards and will meet all the standards. All [the condition of accessibility] but the quality×ta punu[
+                        With the dream of imparting education "Awadh Bihar Singh Mahavalaya Karmanparu" has been established. This teaching
+                        institute with its concern for quality×thermal teaching as well as for all round development of students.
+                        is standing.
+                        Situated in a lush green, unspoiled, sunny environment, this college has a well-equipped,
+                        qualified, experienced lecturer and well-equipped laboratory in our organization. ? It is equipped
+                        with computer and playing facilities with huge study and internet facility. The vehicle will be given.
+                        We will provide a platform to the students to achieve not only education but also co-workers and
+                        excellence in education.
+                        [ A multiple disciplinary study will xilicate Sanskrit
+                        . We invite everyone open to new ideas and suggestions that can connect students and students in
+                        our college on an £-based basis. I welcome young students and wish them success as a responsible
+                        and valuable citizen. I can't believe that if you stay and study in our college, it's always worth the
+                        money for you. [ and chxt
+                        Will be amazing. I wish all the best to all the students who took shelter in this institute.
+                        I wish
+                    </div>
+                    <div className="directors-signature">
+                        <div className="directors-name">Sunil , Awadh Bihar</div>
+                        <div className="directors-signature-img">
+                            <img src="./sign.png"></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default DirectorsNote;
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -6,16 +6,18 @@ import Top_100vh from "../Top-100vh/Top"
 import './Homepage.css'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
+import DirectorsNote from "../DirectorsNote/DirectorsNote"
 
 const Homepage = () => {
     return (  
         <div className="Homepage"  >
             <Navbar active="home"></Navbar>
             <Top_100vh></Top_100vh>
+            <DirectorsNote></DirectorsNote>
             <News></News>
             <Courses></Courses>
             <Announcement></Announcement>
-            <Campus_life></Campus_life>
+            {/* <Campus_life></Campus_life> */}
             <div className="home-footer">
                 <Footer></Footer>
             </div>
